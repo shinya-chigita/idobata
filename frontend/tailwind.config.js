f3a9d8db7d88c6cb022bc84c3e4377f9a9ee0e35
Jun Ito <jujunjun110@gmail.com>
2025-05-01 23:31:07 -0600

    Add: font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,9 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: [
+        '"BIZ UDGothic"',
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "sans-serif",
+      ],
+    },
     extend: {
       fontFamily: {
-        'biz': ['"BIZ UDGothic"', 'sans-serif'],
+        biz: ['"BIZ UDGothic"', "sans-serif"],
       },
     },
   },
