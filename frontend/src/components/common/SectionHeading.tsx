2b384fe67ff2b3eb55db2e971872b049b8ce0a32
Jun Ito <jujunjun110@gmail.com>
2025-05-02 07:04:19 -0600

    Update: 縦揃え修正

--- frontend/src/components/common/SectionHeading.tsx
+++ frontend/src/components/common/SectionHeading.tsx
@@ -8,8 +8,10 @@ interface SectionHeadingProps {
 export function SectionHeading({ title, className }: SectionHeadingProps) {
   return (
     <div className={cn("flex py-2 mb-3", className)}>
-      <div className="w-1 h-6 bg-primary rounded-full mr-2 mt-[0.35rem]" />
-      <h2 className="text-xl font-bold text-foreground font-biz">{title}</h2>
+      <div className="w-1 h-6 bg-primary rounded-full mr-2" />
+      <h2 className="text-xl font-bold text-foreground font-biz leading-6">
+        {title}
+      </h2>
     </div>
   );
 }
