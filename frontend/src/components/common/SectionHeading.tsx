376f1c5f046a266a6b9580967b71fb7f62ec1721
itoz <romatica@gmail.com>
2025-05-05 19:37:04 +0900

    fix #128

--- frontend/src/components/common/SectionHeading.tsx
+++ frontend/src/components/common/SectionHeading.tsx
@@ -8,7 +8,7 @@ interface SectionHeadingProps {
 export function SectionHeading({ title, className }: SectionHeadingProps) {
   return (
     <div className={cn("flex py-2 mb-3 items-center", className)}>
-      <span className="w-[6px] h-8 bg-primary rounded-full mr-2" />
+      <span className="w-2 h-8 bg-primary rounded-sm mr-2" />
       <h2 className="text-2xl md:text-3xl text-foreground font-biz leading-6">
         {title}
       </h2>
