843de906bf796bac77b601627d683883383fc3e1
spinute <spinutids@gmail.com>
2025-05-05 17:30:49 +0900

    adjust like

--- frontend/src/components/question/KeyQuestionHeader.tsx
+++ frontend/src/components/question/KeyQuestionHeader.tsx
@@ -22,44 +22,34 @@ const KeyQuestionHeader = ({
 
   return (
     <div className="mb-6">
-      {tagLine ? (
-        <>
-          <div className="flex justify-start gap-4 mb-4">
-            <h1 className="text-xl md:text-2xl lg:text-3xl font-bold">
-              {tagLine}
-            </h1>
+      <div className="flex justify-start gap-4 mb-4">
+        <h1 className="text-xl md:text-2xl lg:text-3xl font-bold">{tagLine}</h1>
 
-            <button
-              className={`${isLiked ? "text-primary hover:text-primary-400" : "text-neutral-400 hover:text-primary-400"} flex items-center`}
-              type="button"
-              onClick={toggleLike}
-            >
-              <div className="flex items-center">
-                <div className="flex items-center gap-1 border border-neutral-200 rounded-md p-1">
-                  {isLiked ? (
-                    <>
-                      <ThumbsUp className="h-4 w-4" fill="currentColor" />
-                      <span className="text-xs">気になる</span>
-                      <span className="text-xs min-w-2">{displayCount}</span>
-                    </>
-                  ) : (
-                    <>
-                      <ThumbsUp className="h-4 w-4" fill="none" />
-                      <span className="text-xs">気になる</span>
-                      <span className="text-xs min-w-2">{displayCount}</span>
-                    </>
-                  )}
-                </div>
-              </div>
-            </button>
+        <button
+          className={`${isLiked ? "text-primary hover:text-primary-400" : "text-neutral-400 hover:text-primary-400"} flex items-center`}
+          type="button"
+          onClick={toggleLike}
+        >
+          <div className="flex items-center">
+            <div className="flex items-center gap-1 border border-neutral-200 rounded-md p-1">
+              {isLiked ? (
+                <>
+                  <ThumbsUp className="h-4 w-4" fill="currentColor" />
+                  <span className="text-xs">気になる</span>
+                  <span className="text-xs min-w-2">{displayCount}</span>
+                </>
+              ) : (
+                <>
+                  <ThumbsUp className="h-4 w-4" fill="none" />
+                  <span className="text-xs">気になる</span>
+                  <span className="text-xs min-w-2">{displayCount}</span>
+                </>
+              )}
+            </div>
           </div>
-          <p className="text-neutral-500 mb-3">{question}</p>
-        </>
-      ) : (
-        <h1 className="text-xl md:text-2xl lg:text-3xl font-bold mb-4">
-          {question}
-        </h1>
-      )}
+        </button>
+      </div>
+      <p className="text-neutral-500 mb-3">{question}</p>
 
       <div className="flex flex-wrap gap-3 text-xs text-neutral-500 mb-4">
         {tags && tags.length > 0 && (
