a4d864bf16a39b18ece364b72ebb95be49bbe4ce
Jun Ito <jujunjun110@gmail.com>
2025-07-24 09:35:43 +0900

    Update: del unnecessary imports

--- frontend/src/components/ui/radio.tsx
+++ frontend/src/components/ui/radio.tsx
@@ -1,4 +1,4 @@
-import { type VariantProps, cva } from "class-variance-authority";
+import { cva } from "class-variance-authority";
 import * as React from "react";
 import { cn } from "../../lib/utils";
 
