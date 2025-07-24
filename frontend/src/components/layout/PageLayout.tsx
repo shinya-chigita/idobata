7b6e9bfc1066c32df782206cd1903119f71c6701
Jun Ito <jujunjun110@gmail.com>
2025-07-24 09:15:14 +0900

    Delete: sidebar

--- frontend/src/components/layout/PageLayout.tsx
+++ frontend/src/components/layout/PageLayout.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import Sidebar from "./Sidebar";
 
 interface PageLayoutProps {
   children: ReactNode;
@@ -11,8 +10,7 @@ const PageLayout = ({ children }: PageLayoutProps) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <Sidebar />
-      <main className="flex-grow md:ml-[260px] px-4 md:px-8">{children}</main>
+      <main className="flex-grow px-4 md:px-8">{children}</main>
       <Footer />
     </div>
   );
